bf2ec520db7b2e3176cbe0ac9a358b750befc14e
Andrew Pahomov <pahomov.kch@gmail.com>
2021-11-07 21:16:22 +0500

    Eslint action done

--- src/common/Tag/index.tsx
+++ src/common/Tag/index.tsx
@@ -1,6 +1,6 @@
 import cn from 'classnames'
-import styles from './index.module.css'
 import {memo} from 'react'
+import styles from './index.module.css'
 
 type TagProps = {
   children: React.ReactNode
